0b1f31876c9b089f7924fbc6e1af5f036d68c308
Balkı <balki1985@gmail.com>
2020-11-12 21:40:35 +0300

    Initial

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
-//#include <stdio.h>
-//#include <mpi.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//void main(){
-//    int size = 4;
-//    int process_count = 2;
-//    int epochs = size * size * process_count;
-//
-//    for (int epoch = 1; epoch <= epochs; epoch++) {
-//        int row2 = (epoch - 1) % size;
-//        int col2 = ((epoch - 1) / (size / process_count)) % size;
-//
-//        printf("Epoch: %d\t[%d, %d]\n", epoch, row2, col2);
-//    }
-//}
-//
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(){
+    int size = 4;
+    int process_count = 1;
+    int epochs = size * size * process_count;
+
+    for (int epoch = 1; epoch <= epochs; epoch++) {
+        int row2 = ((epoch - 1) % (size / process_count)) * (size / process_count);
+        int col2 = ((epoch - 1) / (size / process_count)) % size;
+
+        printf("Epoch: %d\t[%d, %d]\n", epoch, row2, col2);
+    }
+}
+
 ////1   0   0     0   0     0   0
 ////2   1   0     2   0     0   1
 ////3   2   0     0   1     0   2
